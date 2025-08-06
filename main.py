c7b80780d0180c92d43617b59201e642360ab51d
andryushkakolesnikov <kolesnikow.andrei13@gmail.com>
2025-08-06 17:14:47 +0300

    Improve input validation

--- main.py
+++ main.py
@@ -14,8 +14,15 @@ class PasswordGenerator():
                 break
             else:
                 print('Недопустимое значение параметра попробуйте ещё раз')
-        return input_value
-
+        if not input_value.isdigit():
+            input_value = input_value.lower()
+            if input_value == 'да':
+                return True
+            else:
+                return False
+        else:
+            return int(input_value)
+        
     
     def get_user_input(self):
         """Реализует диалог с пользователем и сохранияет настройки
@@ -30,9 +37,34 @@ class PasswordGenerator():
         self.available_uppercase_letters = \
             self.check_user_input('Включать ли заглавные буквы? (да/нет)',
                                   avalible_answer)
-        self.avaliable_digits = self.check_user_input('Включать ли цифры? (да/нет)',
+        self.available_digits = self.check_user_input('Включать ли цифры? (да/нет)',
                                                        avalible_answer)
+        self.available_special_char = self.check_user_input('Включать ли специальные символы? (да/нет)', 
+                                                            avalible_answer)
         
-if __name__ == "__main__":
+
+    def check_categories_char(self):
+        categories = [
+            self.available_lowercase_letters,
+            self.available_uppercase_letters,
+            self.available_digits,
+            self.available_special_char
+        ]
+        if any(categories):
+            return True
+        else:
+            print('Для генерации пароля выберите хотя-бы одну категорию символов')
+            return False
+            
+
+def main():
     object_PasswordGenerator = PasswordGenerator()
-    object_PasswordGenerator.get_user_input()
+    while True:
+        object_PasswordGenerator.get_user_input()
+        flag = object_PasswordGenerator.check_categories_char()
+        if flag:
+            break
+    
+
+if __name__ == "__main__":
+    main()
